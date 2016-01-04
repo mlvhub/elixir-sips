645e2f629af71372c69aa3c41965f23e6cee6ec8
Miguel Lopez V <mlopezva@akamai.com>
2016-01-04 01:33:43 -0500

    Done with Episode 30

---
+++ exactor_test/lib/count_actor.ex
@@ -0,0 +1,11 @@
+defmodule ExactorTest.CountActor do
+  use ExActor.GenServer, export: :counter
+
+  defstart start(x), do: initial_state(x)
+
+  defcall get, state: 2, do: reply(:two)
+  defcall get, state: state, do: reply(state)
+  
+  defcast inc, state: state, do: new_state(state + 1)
+  defcast dec, state: state, do: new_state(state - 1)
+end
